6ecdac51c8cfac7702082d2895c62d2ead0d60a5
AnjumolSree <anjumolsreedaran@gmail.com>
2023-10-06 16:09:03 +0530

    Javacript Ninja

---
+++ anjumol/Javascript/dom.js
@@ -0,0 +1,51 @@
+// // // const para = document.querySelector('div.error');
+
+// // // console.log(para);
+
+// // const paras =document.querySelectorAll('p');
+// // const errors =document.querySelectorAll('.error');
+
+// // // paras.forEach(para =>{
+// // //     console.log(para);
+// // // })
+// // console.log(errors);
+
+// /*--------------------------------------------*/
+
+// //get an element by ID
+
+// // const title  = document.getElementById('page-title');
+// // console.log(title);
+
+// // //get an element by their class name
+
+// // const errors  = document.getElementsByClassName('error');
+// // console.log(errors);
+// // console.log(errors[0]);
+
+// // //get elements by their tag name
+
+// // const paras  = document.getElementsByTagName('p');
+// // console.log(paras);
+ 
+// /*------------------------------------------*/
+
+// const para = document.querySelector('p');
+// // console.log(para.innerText);
+// // para.innerText = "It is awsome";
+
+// const paras = document.querySelectorAll('p');
+// // paras.forEach(para =>{
+// //     console.log(para.innerText);
+// //     para.innerText += 'New text';
+
+// // });
+
+// const content = document.querySelector('.content');
+// // console.log(content.innerHTML);
+// //content.innerHTML = '<h2>THIS IS A NEW H2<h2>';
+
+// const people = ['Mario','Luigi','Yoshi'];
+// people.forEach(person =>{
+//     content.innerHTML +=`<p>${person}<p>`;
+// });
