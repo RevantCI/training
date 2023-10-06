1041861f63d9edf10ebcb070ffa5092cac4de3e3
AnjumolSree <anjumolsreedaran@gmail.com>
2023-10-06 10:27:23 +0530

    Javacript Ninja

---
+++ anjumol/Javascript/types.js
@@ -0,0 +1,194 @@
+/*let age = 25;
+let year = 2019;
+
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+points= 50;
+console.log(points);*/
+/*
+//Strings
+console.log('hello world');
+
+let email='mario@gmail.com';
+console.log(email);
+
+//String concatination
+let firstName = 'Aron';
+let lastName = 'Rony';
+
+let fullName= firstName + ' ' + lastName;
+console.log(fullName);
+
+//getting characters
+console.log(fullName[0]);
+
+//String Length
+console.log(fullName.length);
+
+//String Methods
+
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result,fullName);
+
+
+let index = firstName.indexOf('@');
+console.log(email);
+*/
+//let email ='mario@thenetninja.co.uk';
+//let result =email.lastIndexOf('n');
+
+//let result=email.slice(0,10);
+//let result=email.substr(0,10);
+
+//let result = email.replace('m','w');*/
+
+//console.log(result);
+
+
+//let radius =10;
+//const pi =3.14;
+
+//console.log(radius,pi);
+
+//Math Operators +,-,*,/,**,%
+
+//console.log(10/2);
+//let result =radius%3;
+
+//let result =pi*radius**2;
+
+//Order of Operation B I D M A s
+
+//let result = 5*(10-3)**2;
+//console.log(result);
+
+//let likes = 10;
+//likes=likes+1;
+
+/*likes++;
+likes--;
+likes += 10;
+likes -= 5;
+likes *=2;
+likes/=2;*/
+
+//console.log(likes);
+
+//Nan -- Not a number
+//console.log(5/'hello');
+
+//let result = 'the blog has' + likes +'likes';
+//console.log(result);
+
+//Template Strings
+
+//const title = 'Best reads of 2019';
+//const author =' Mario';
+//const likes = 30;
+
+//concatination way
+//let result = 'The blog called'+ title + 'by' + author + 'has' + likes + 'likes';
+//console.log(result);
+//Template string way
+//let result = `The blog called ${title} by ${author} has ${likes} likes`;
+//onsole.log(result);
+//Creating HTML templates
+
+//let html = `
+//<h2>${title}</h2>
+//<p>By${author}</p>
+//<span>This blog has ${likes} likes </span>
+//`;
+//console.log(html);
+
+
+//let ninjas =['shaun','ryu','chun-li'];
+//ninjas[1] ='ken';
+//console.log(ninjas[1]);
+
+//let ages=[20,25,30,35];
+//console.log(ages[2]);
+      
+
+//let random =['shaun','crystal',30,20];
+//console.log(random);
+
+//console.log(ninjas.length);
+
+//Array Methods
+
+//let result = ninjas.join('-');
+//let result = ninjas.indexOf('chun-li');
+//let result = ninjas.concat(['ken','crystal']);
+//let result = ninjas.push('ken');
+
+//console.log(result);
+
+//let age = null;
+//console.log(age,age+3,`the age is ${age}`);
+
+
+//Boolean & Comparisons
+//console.log(true,false,"true","false");
+
+//Methods can return Booleans
+//let email ="luigi@thenetninja.co.uk";
+//let names = ['mario','luigi','toad'];
+
+
+//let result = email.includes('!');
+//let result  = names.includes('luigi');
+//console.log(result);
+
+
+//Comparison Operator
+
+// let age = 25;
+/*console.log(age == 25);
+console.log(age == 30);
+console.log(age != 30);
+console.log(age > 20);
+console.log(age < 20);
+console.log(age <= 20);
+console.log(age >= 20);*/
+
+/*let name = 'shaun';
+console.log(name == 'shaun');
+console.log(name == 'Shaun');
+console.log(name > 'crystal');
+console.log(name > 'Shaun');
+console.log(name >'Crystal');*/
+
+//loose comparison (different types can still be equal)
+//console.log(age == 25);
+//console.log(age == '25');
+//console.log(age != 25);
+//console.log(age != '25');
+
+//strict comparison (different type cannot be equl)
+
+//console.log(age === 25);
+//console.log(age === '25');
+//console.log(age !== 25);
+//console.log(age !== '25');
+
+
+//type conversion
+
+// let score = '100';
+// //score = Number(score);
+// console.log(score +1);
+// console.log(typeof score);
+
+
+// let result = Number('hello');
+// let result = String(50);
+// let result = Boolean(100);
+// let result = Boolean('0');
+// console.log(result,typeof result);
+
